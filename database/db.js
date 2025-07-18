8f1068812e81c815fe8570992111bd7ec3337e9c
minimi7401 <minimi7401@gmail.com>
2025-07-18 14:55:34 +0900

    a

--- database/db.js
+++ database/db.js
@@ -8,8 +8,8 @@ export const connectDB = async () => {
     if (db) return db;
 
     // DB연결후 해당 DB반환
-    // const MONGODB_URI = process.env.NODE_ENV === "development" ? process.env.MONGODB_URI_LOCAL : MONGODB_URI_ATLAS;
-    const MONGODB_URI = MONGODB_URI_ATLAS
+    // const MONGODB_URI = process.env.NODE_ENV === "development" ? process.env.MONGODB_URI_LOCAL : process.env.MONGODB_URI_ATLAS;
+    const MONGODB_URI = process.env.MONGODB_URI_ATLAS
 
 
     const client = new MongoClient(MONGODB_URI);
